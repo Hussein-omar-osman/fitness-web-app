0fe4e63f72a7abfed01fa083602a39ac710434bb
Hussein Omar <husseinomar6190@gmail.com>
2022-07-18 15:32:13 +0300

    creating the basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './componets/Navbar';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from './componets/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello react</h1>
-    </div>
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
