0629c93b1bfd3be92422057275eab748cb277541
Hussein Omar <husseinomar6190@gmail.com>
2022-07-19 18:38:51 +0300

    adding loader/spinner while fetching data from the api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import SearchExercises from '../componets/SearchExercises';
 import Exercises from '../componets/Exercises';
 import HeroBanner from '../componets/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
